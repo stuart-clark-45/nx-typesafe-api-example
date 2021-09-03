61e113759371f8033f65c1cf5d6a7e3daaecbfe0
Stuart Clark <stuart@webuildbots.ai>
2021-09-03 18:15:24 +0100

    import backend code

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,1 +1,32 @@
-console.log('Hello World!');
+import express, { RequestHandler } from 'express';
+import { addRoute, ExpressRoute } from 'typesafe-api';
+import { helloWorldController } from './app/hello-world';
+import {
+  helloWoldRoute,
+  HelloWorldEndpointDef,
+} from '@nx-typesafe-api-example/api-spec';
+import cors from 'cors';
+import { authorize } from './app/authorize';
+
+const app = express();
+
+app.use(cors(), express.json());
+
+// Define the middleware we want for the route
+const middleware: RequestHandler[] = [authorize()];
+
+// Import the route from the api-spec then add the additional fields needed for an {@link ExpressRoute}
+const eHelloWorldRoute: ExpressRoute<HelloWorldEndpointDef> = {
+  ...helloWoldRoute,
+  middleware,
+  controller: helloWorldController,
+};
+
+// Add the route to the express app
+addRoute(app, eHelloWorldRoute);
+
+// Start the server
+const port = 7809;
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
