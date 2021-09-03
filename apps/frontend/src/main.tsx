af3e554d39860f0bcc3a671145522aefe6a1b21d
Stuart Clark <stuart@webuildbots.ai>
2021-09-03 21:15:49 +0100

    implement example

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,14 +1,24 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
+import styled from 'styled-components';
+
+const Html = styled.html`
+  height: 100vh;
+  box-sizing: border-box;
+`;
 
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Html>
+        <body>
+          <App />
+        </body>
+      </Html>
     </BrowserRouter>
   </StrictMode>,
   document.getElementById('root')
