4e97b5c194e67300123f8928f641719c3197d08e
stuart <stuart@strt-solutions.com>
2022-09-01 14:10:02 +0700

    rename backend to express

--- apps/express/src/app/hello-world.ts
+++ apps/express/src/app/hello-world.ts
@@ -1,32 +1,27 @@
-import {Controller, sendError, TRequest, TResponse} from 'typesafe-api';
-import {HelloWorldEndpointDef} from '@nx-typesafe-api-example/api-spec';
-
+import { Controller, sendError, TRequest, TResponse } from 'typesafe-api';
+import { HelloWorldEndpointDef } from '@nx-typesafe-api-example/api-spec';
 
 export const helloWorldController: Controller<HelloWorldEndpointDef> = (
   req: TRequest<HelloWorldEndpointDef>,
   res: TResponse<HelloWorldEndpointDef>
 ) => {
-
   // `req.query` is typesafe so you know which keys have been set in the request
   const name = req.query.yourName;
 
   // As an example, let's return an error the name parameter is a number
   const isNumber = (s: string) => /^\d+$/.test(s);
   if (isNumber(name)) {
-
     // This error object is typesafe, including the status so you can only select from the
     // statuses given in the endpoint definition
     return sendError(res, {
       status: 400,
-      msg: "Surely your name isn't a number?? 😵"
+      msg: "Surely your name isn't a number?? 😵",
     });
-
   }
 
   // No surprises, this body is typesafe too!
   res.send({
     msg: `Hello ${name}!`,
-    date: new Date()
-  })
-
+    date: new Date(),
+  });
 };
