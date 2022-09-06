893db170d6a11275f1af429ac0ddfb011112a25f
stuart <stuart@strt-solutions.com>
2022-09-07 07:53:32 +0800

    update to nx 14

--- apps/express/jest.config.ts
+++ apps/express/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'serverless',
+/* eslint-disable */
+export default {
+  displayName: 'express',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +12,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/serverless',
+  coverageDirectory: '../../coverage/apps/express',
 };
--- libs/api-spec/jest.config.ts
+++ libs/api-spec/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'api-spec',
   preset: '../../jest.preset.js',
   globals: {
