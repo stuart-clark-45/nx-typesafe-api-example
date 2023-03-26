7ea65128cdd06fce1d8fcd3020f384093756804e
stuart <stuart@strt-solutions.com>
2023-03-26 12:57:02 +0100

    libs/api-spec/src/routes/hello-world.ts: correct error type

--- libs/api-spec/src/routes/hello-world.ts
+++ libs/api-spec/src/routes/hello-world.ts
@@ -1,11 +1,11 @@
-import {ErrorType, ReqOptions, ResOptions, Route} from '@typesafe-api/core';
-import {DefaultErrorCodes, ExampleApiEndpoint} from '../api';
+import { ReqOptions, ResOptions, Route } from '@typesafe-api/core';
+import { ApiErrorType, DefaultErrorCodes, ExampleApiEndpoint } from '../api';
 
 // Define the all parameters that are required to make the request
 export interface HelloWorldReq extends ReqOptions {
   query: {
     yourName: string;
-  }
+  };
 }
 
 // Define the response type we wil receive for the request
@@ -13,20 +13,24 @@ export interface HelloWorldResp extends ResOptions {
   body: {
     msg: string;
     date: Date;
-  }
+  };
   headers: {
     example: string;
-  }
+  };
 }
 
 // Define any error that may be thrown by the endpoint, the default is just `500`
-export type HelloWorldErrors = ErrorType<DefaultErrorCodes|400>
+export type HelloWorldErrors = ApiErrorType<DefaultErrorCodes | 400>;
 
 // Create the endpoint definition this type encapsulates the full endpoint spec
-export type HelloWorldEndpointDef = ExampleApiEndpoint<HelloWorldReq, HelloWorldResp, HelloWorldErrors>
+export type HelloWorldEndpointDef = ExampleApiEndpoint<
+  HelloWorldReq,
+  HelloWorldResp,
+  HelloWorldErrors
+>;
 
 // Define the route at which the endpoint belongs
 export const helloWoldRoute: Route<HelloWorldEndpointDef> = {
   method: 'get',
-  path: '/hello-world'
+  path: '/hello-world',
 };
