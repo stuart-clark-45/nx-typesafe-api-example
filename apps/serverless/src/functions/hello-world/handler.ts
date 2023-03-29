3005b57d5d8401555d0f9ffde27e81f86ea7dad7
stuart <stuart@strt-solutions.com>
2023-03-29 07:58:21 +0100

    apps/serverless/src/functions/hello-world/handler.ts: update for v3

--- apps/serverless/src/functions/hello-world/handler.ts
+++ apps/serverless/src/functions/hello-world/handler.ts
@@ -1,23 +1,28 @@
-import {createError, createHandler, TypesafeApiHandler} from '@typesafe-api/serverless';
-import { HelloWorldEndpointDef } from '@nx-typesafe-api-example/api-spec';
+import {
+  typesafeApi,
+  typesafeApiErrors,
+  TypesafeApiHandler,
+} from '@typesafe-api/serverless';
+import {
+  HelloWorldEndpointDef,
+  throwHttpError,
+} from '@nx-typesafe-api-example/api-spec';
+import middy from '@middy/core';
 
-export const typesafeHandler: TypesafeApiHandler<HelloWorldEndpointDef> = async (
+const handlerFn: TypesafeApiHandler<HelloWorldEndpointDef> = async (
   event,
   context
 ) => {
-
   // `req.query` is typesafe so you know which keys have been set in the request
   const name = event.typesafeApi.query.yourName;
 
   // As an example, let's return an error the name parameter is a number
   const isNumber = (s: string) => /^\d+$/.test(s);
   if (isNumber(name)) {
-    // This error object is typesafe, including the status so you can only select from the
-    // statuses given in the endpoint definition
-    return createError({
-      status: 400,
-      msg: "Surely your name isn't a number?? 😵",
-    });
+    throwHttpError<HelloWorldEndpointDef>(
+      400,
+      "Surely your name isn't a number?? 😵"
+    );
   }
 
   return {
@@ -25,8 +30,24 @@ export const typesafeHandler: TypesafeApiHandler<HelloWorldEndpointDef> = async 
     body: {
       msg: `Hello ${name} from a serverless backend`,
       date: new Date(),
-    }
+    },
   };
 };
 
-export const handler = createHandler<HelloWorldEndpointDef>(typesafeHandler);
+// Finally we need to add the typesafe api middleware to our handler. It can be useful to create a
+// function manage this and any other standard middleware your handlers use.
+export const handler = middy(handlerFn)
+  // Add the typesafe api middleware, this is responsible for parsing requests
+  .use(typesafeApi())
+  // Error handling middleware
+  .use(
+    typesafeApiErrors({
+      // Custom logging functions can be added here, by default console.log will be used
+      httpErrorLogFn: undefined,
+      otherErrorLogFn: undefined,
+      // The default error body that will be returned to users when an unhandled error occurs
+      internalServerErrorBody: {
+        msg: 'Internal server error',
+      },
+    })
+  );
