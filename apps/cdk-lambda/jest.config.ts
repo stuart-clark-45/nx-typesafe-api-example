f78a37f5b1348b0f638c67b9984882a5fb44801d
stuart <stuart@strt-solutions.com>
2023-03-31 07:02:57 +0100

    apps/cdk-lambda: created

---
+++ apps/cdk-lambda/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'cdk-lambda',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/cdk-lambda',
+};
