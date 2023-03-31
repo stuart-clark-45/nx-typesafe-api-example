3d90b07014e2c0ef34b88bd3f9314e91b286c49e
stuart <stuart@strt-solutions.com>
2023-03-31 07:43:47 +0100

    libs/lambda-handlers -> libs/lambda-functions

---
+++ libs/lambda-functions/README.md
@@ -0,0 +1,11 @@
+# lambda-functions
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test lambda-functions` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint lambda-functions` to execute the lint via [ESLint](https://eslint.org/).
--- libs/lambda-functions/jest.config.ts
+++ libs/lambda-functions/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'lambda-handlers',
+  displayName: 'lambda-functions',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/lambda-handlers',
+  coverageDirectory: '../../coverage/libs/lambda-functions',
 };
